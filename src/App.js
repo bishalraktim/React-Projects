5667f9b07ec7620a95bd9744f446736efa7c7fcb
bishalraktim <raktimbishal@gmail.com>
2020-05-13 08:00:04 +1000

    Handle dynamic content

--- src/App.js
+++ src/App.js
@@ -56,6 +56,29 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Raktim!!!')}
+            change={this.changeEventHandler}>My Hobbies: Playing Chess
+          </Person>
+
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );  
+    }
+
     return (
       <div className="App">
         <h1>Learning React from Scratch!</h1>
@@ -65,25 +88,7 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons
         </button>
 
-        {
-          this.state.showPersons === true ?
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Raktim!!!')}
-              change={this.changeEventHandler}>My Hobbies: Playing Chess
-            </Person>
-
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-          </div> : null
-        }
+        {persons}
 
       </div>
     );
